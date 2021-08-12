47c5275a0f1fa6f5ea6837ed17520ab5a736e223
shunwitter <3123900+shunwitter@users.noreply.github.com>
2021-08-12 18:15:26 +0900

    Implement react-navigation 6

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Go to PhotoUploader"
+        onPress={() => { navigation.navigate('PhotoUploader'); }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
